c22a42f309d49dd41473f829de3d964ccef420d1
Enes Kamis <eneskamis@yandex.com>
2020-04-09 02:25:31 +0300

    E-Park_v5.2
    Animasyon sayfası eklendi ve çalışır pozisyona getirildi.
    Renk ve fontlarda değişim yapıldı.

--- E_Park/app/src/main/java/com/example/e_park/AnimasyonActivity.java
+++ E_Park/app/src/main/java/com/example/e_park/AnimasyonActivity.java
@@ -2,13 +2,32 @@ package com.example.e_park;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class AnimasyonActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animasyon);
+        progressBar = findViewById(R.id.progressBar);
+
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2200);
+                    Intent intent = new Intent(getApplicationContext(),GirisSayfasiActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
     }
 }
